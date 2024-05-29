c38d7ba6ea5ebb0c5d04004e3e78a59051265a27
Eduardo Vargas <eduardo704@gmail.com>
2024-05-29 16:57:04 -0300

    make seed picture static

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -55,7 +55,7 @@ async function generateAccomodations(user: any) {
     },
   ];
 
-  for (let index = 0; index < 20; index++) {
+  for (let index = 0; index < 8; index++) {
     const level = generateLevel();
     const picture =
       "https://images.unsplash.com/photo-1585567512124-dbfaa0e7eee5?crop=entropy&cs=srgb&fm=jpg&ixid=M3w1NTg2NTF8MHwxfHNlYXJjaHwxfHxzdXJmJTIwaG91c2V8ZW58MHwyfHx8MTcwNjIwOTYxNnww&ixlib=rb-4.0.3&q=85";
